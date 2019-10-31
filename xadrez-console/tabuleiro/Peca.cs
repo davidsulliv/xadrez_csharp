0c538dbc72a5a09fa37f2989769d7cd4983a886a
arch765 <sullivan.davidson@gmail.com>
2019-10-31 16:56:04 -0300

    Implementando a verificacao de xeque

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -20,6 +20,11 @@
             QteMovimentos++;
         }
 
+        public void decrementaQteMovimentos()
+        {
+            QteMovimentos--;
+        }
+
         public bool existeMovimentosPossiveis()
         {
             bool[,] mat = movimentosPossiveis();
